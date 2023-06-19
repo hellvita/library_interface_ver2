5ddf68ec7984ba7b00a5362e40f4ccfb732b5e5a
Olga <seredaolga03@gmail.com>
2023-06-19 17:27:17 +0200

    Create class DataBase.cs

---
+++ library_interface_ver2/DataBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace library_interface_ver2
+{
+    class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=127.0.0.1;uid=LocalUser1;pwd=User1;database=library");
+
+        public void openConenection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed) { connection.Open(); }
+        }
+
+        public void closeConenection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) { connection.Close(); }
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
