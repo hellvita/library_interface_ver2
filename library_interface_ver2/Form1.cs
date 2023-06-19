059d73cfedb0953421622b194f5e1490234c9cc5
Olga <seredaolga03@gmail.com>
2023-06-19 17:57:53 +0200

    Create form Form1.cs

--- library_interface_ver2/Form1.cs
+++ library_interface_ver2/Form1.cs
@@ -7,15 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace library_interface_ver2
 {
     public partial class Form1 : Form
     {
+        DataBase database = new DataBase();
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
