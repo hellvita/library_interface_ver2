ccbcb39753693b90b0b91d7569cdf9b2ea86103c
Olga <seredaolga03@gmail.com>
2023-06-19 22:44:46 +0200

    Add method to check valid of password in UserSinginForm.cs

--- library_interface_ver2/UserSinginForm.cs
+++ library_interface_ver2/UserSinginForm.cs
@@ -43,21 +43,24 @@ namespace library_interface_ver2
             var userEmail = textBox_user_email.Text;
             var userPwd = md5.hashPwd(textBox_user_pwd.Text);
 
-            if (DataIsOK(userFullName, userPhone, userEmail)) {
+            if (DataIsOK(userFullName, userPhone, userEmail, textBox_user_pwd.Text)) {
 
                 CloseThisForm();
             }
         }
 
-        private bool DataIsOK(string name, string phone, string email) {
+        private bool DataIsOK(string name, string phone, string email, string pwd) {
             bool nameOK = false;
             bool phoneOK = false;
             bool emailOK = false;
+            bool passwordOK = false;
 
             nameOK = nameIsOK(name);
             phoneOK = phoneIsOK(phone);
+            emailOK = emailIsOK(email);
+            passwordOK = pwdIsOK(pwd);
 
-            if (nameOK && phoneOK && emailOK) return true;
+            if (nameOK && phoneOK && emailOK && passwordOK) return true;
             return false;
         }
 
@@ -77,8 +80,6 @@ namespace library_interface_ver2
         }
 
         private bool phoneIsOK(string phone) {
-
-
             if (phone != "" && phone != "+380 000 000 00 00") {
                 textBox_user_phone.ForeColor = Color.Black;
                 string query_str = $"SELECT PHONE FROM users WHERE PHONE = '{phone}'";
@@ -94,6 +95,35 @@ namespace library_interface_ver2
             }
             textBox_user_phone.ForeColor = Color.Red;
             textBox_user_phone.Text = "+380 000 000 00 00";
+            return false;
+        }
+
+        private bool emailIsOK(string email) {
+            if (email != "" && email != "hisentia@gmail.com")
+            {
+                textBox_user_email.ForeColor = Color.Black;
+                string query_str = $"SELECT EMAIL FROM users WHERE EMAIL = '{email}'";
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                DataTable table = new DataTable();
+
+                MySqlCommand command = new MySqlCommand(query_str, database.GetConnection());
+
+                adapter.SelectCommand = command;
+                adapter.Fill(table);
+                if (table.Rows.Count == 0) return true;
+            }
+            textBox_user_email.ForeColor = Color.Red;
+            textBox_user_email.Text = "hisentia@gmail.com";
+            return false;
+        }
+
+        private bool pwdIsOK(string pwd) {
+            int count = 0;
+            if (pwd != "") {
+                foreach (var c in pwd) { count++; }
+                if (count >= 5) { return true; }
+            }
             return false;
         }
 
