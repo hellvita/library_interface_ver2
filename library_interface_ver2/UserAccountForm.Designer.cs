870f9559632de33dc0d499c6f72bed89bd459351
Olga <seredaolga03@gmail.com>
2023-06-20 21:56:19 +0200

    Design UserAccountForm.cs

--- library_interface_ver2/UserAccountForm.Designer.cs
+++ library_interface_ver2/UserAccountForm.Designer.cs
@@ -30,10 +30,22 @@ namespace library_interface_ver2
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.button_editEmail = new System.Windows.Forms.Button();
+            this.button_editPhone = new System.Windows.Forms.Button();
+            this.button_editFname = new System.Windows.Forms.Button();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_phone = new System.Windows.Forms.TextBox();
+            this.textBox_fullName = new System.Windows.Forms.TextBox();
+            this.textBox_userID = new System.Windows.Forms.TextBox();
+            this.label_email = new System.Windows.Forms.Label();
+            this.label_phone = new System.Windows.Forms.Label();
+            this.label_fullName = new System.Windows.Forms.Label();
+            this.label_userID = new System.Windows.Forms.Label();
             this.button_back = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_deleteAcc = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -41,45 +53,140 @@ namespace library_interface_ver2
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(140)))), ((int)(((byte)(161)))));
+            this.panel1.Controls.Add(this.button_deleteAcc);
+            this.panel1.Controls.Add(this.button_editEmail);
+            this.panel1.Controls.Add(this.button_editPhone);
+            this.panel1.Controls.Add(this.button_editFname);
+            this.panel1.Controls.Add(this.textBox_email);
+            this.panel1.Controls.Add(this.textBox_phone);
+            this.panel1.Controls.Add(this.textBox_fullName);
+            this.panel1.Controls.Add(this.textBox_userID);
+            this.panel1.Controls.Add(this.label_email);
+            this.panel1.Controls.Add(this.label_phone);
+            this.panel1.Controls.Add(this.label_fullName);
+            this.panel1.Controls.Add(this.label_userID);
             this.panel1.Controls.Add(this.button_back);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(812, 600);
+            this.panel1.Size = new System.Drawing.Size(1008, 600);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // button_editEmail
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Pristina", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(43)))), ((int)(((byte)(35)))));
-            this.label1.Location = new System.Drawing.Point(118, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(222, 88);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Hisentia";
+            this.button_editEmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_editEmail.Location = new System.Drawing.Point(956, 319);
+            this.button_editEmail.Name = "button_editEmail";
+            this.button_editEmail.Size = new System.Drawing.Size(45, 42);
+            this.button_editEmail.TabIndex = 18;
+            this.button_editEmail.Text = "✏️";
+            this.button_editEmail.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // button_editPhone
             // 
-            this.pictureBox1.Image = global::library_interface_ver2.Properties.Resources.lib_icon;
-            this.pictureBox1.Location = new System.Drawing.Point(346, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(88, 87);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.button_editPhone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_editPhone.Location = new System.Drawing.Point(956, 258);
+            this.button_editPhone.Name = "button_editPhone";
+            this.button_editPhone.Size = new System.Drawing.Size(45, 42);
+            this.button_editPhone.TabIndex = 17;
+            this.button_editPhone.Text = "✏️";
+            this.button_editPhone.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // button_editFname
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(465, 37);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(312, 42);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Особистий кабінет";
+            this.button_editFname.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_editFname.Location = new System.Drawing.Point(957, 193);
+            this.button_editFname.Name = "button_editFname";
+            this.button_editFname.Size = new System.Drawing.Size(45, 42);
+            this.button_editFname.TabIndex = 16;
+            this.button_editFname.Text = "✏️";
+            this.button_editFname.UseVisualStyleBackColor = true;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_email.Location = new System.Drawing.Point(383, 319);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(567, 42);
+            this.textBox_email.TabIndex = 15;
+            this.textBox_email.Text = "dudka@gmail.com";
+            this.textBox_email.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_phone
+            // 
+            this.textBox_phone.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_phone.Location = new System.Drawing.Point(383, 258);
+            this.textBox_phone.Name = "textBox_phone";
+            this.textBox_phone.Size = new System.Drawing.Size(567, 42);
+            this.textBox_phone.TabIndex = 14;
+            this.textBox_phone.Text = "+380 974 401 96 74";
+            this.textBox_phone.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_fullName
+            // 
+            this.textBox_fullName.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_fullName.Location = new System.Drawing.Point(383, 193);
+            this.textBox_fullName.Name = "textBox_fullName";
+            this.textBox_fullName.Size = new System.Drawing.Size(567, 42);
+            this.textBox_fullName.TabIndex = 13;
+            this.textBox_fullName.Text = "Дудка Бубка Шутка";
+            this.textBox_fullName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox_userID
+            // 
+            this.textBox_userID.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox_userID.Location = new System.Drawing.Point(383, 132);
+            this.textBox_userID.Name = "textBox_userID";
+            this.textBox_userID.Size = new System.Drawing.Size(567, 42);
+            this.textBox_userID.TabIndex = 12;
+            this.textBox_userID.Text = "12345";
+            this.textBox_userID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label_email
+            // 
+            this.label_email.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(154)))), ((int)(((byte)(173)))));
+            this.label_email.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_email.Location = new System.Drawing.Point(24, 317);
+            this.label_email.Name = "label_email";
+            this.label_email.Size = new System.Drawing.Size(353, 47);
+            this.label_email.TabIndex = 11;
+            this.label_email.Text = "Поштова скринька:";
+            this.label_email.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_phone
+            // 
+            this.label_phone.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(154)))), ((int)(((byte)(173)))));
+            this.label_phone.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_phone.Location = new System.Drawing.Point(24, 253);
+            this.label_phone.Name = "label_phone";
+            this.label_phone.Size = new System.Drawing.Size(353, 47);
+            this.label_phone.TabIndex = 10;
+            this.label_phone.Text = "Номер телефону:";
+            this.label_phone.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_fullName
+            // 
+            this.label_fullName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(154)))), ((int)(((byte)(173)))));
+            this.label_fullName.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_fullName.Location = new System.Drawing.Point(24, 191);
+            this.label_fullName.Name = "label_fullName";
+            this.label_fullName.Size = new System.Drawing.Size(353, 47);
+            this.label_fullName.TabIndex = 9;
+            this.label_fullName.Text = "ПІБ (повністю): ";
+            this.label_fullName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_userID
+            // 
+            this.label_userID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(154)))), ((int)(((byte)(173)))));
+            this.label_userID.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label_userID.Location = new System.Drawing.Point(24, 132);
+            this.label_userID.Name = "label_userID";
+            this.label_userID.Size = new System.Drawing.Size(353, 47);
+            this.label_userID.TabIndex = 8;
+            this.label_userID.Text = "№ читацького квитка (логін):";
+            this.label_userID.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // button_back
             // 
@@ -92,11 +199,52 @@ namespace library_interface_ver2
             this.button_back.UseVisualStyleBackColor = true;
             this.button_back.Click += new System.EventHandler(this.button_back_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(465, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Особистий кабінет";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::library_interface_ver2.Properties.Resources.lib_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 87);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Pristina", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(43)))), ((int)(((byte)(35)))));
+            this.label1.Location = new System.Drawing.Point(118, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 88);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hisentia";
+            // 
+            // button_deleteAcc
+            // 
+            this.button_deleteAcc.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button_deleteAcc.Location = new System.Drawing.Point(956, 37);
+            this.button_deleteAcc.Name = "button_deleteAcc";
+            this.button_deleteAcc.Size = new System.Drawing.Size(45, 42);
+            this.button_deleteAcc.TabIndex = 19;
+            this.button_deleteAcc.Text = "🗑️";
+            this.button_deleteAcc.UseVisualStyleBackColor = true;
+            // 
             // UserAccountForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 624);
+            this.ClientSize = new System.Drawing.Size(1026, 624);
             this.Controls.Add(this.panel1);
             this.Name = "UserAccountForm";
             this.Text = "UserAccountForm";
@@ -115,5 +263,17 @@ namespace library_interface_ver2
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button_back;
+        private System.Windows.Forms.Label label_email;
+        private System.Windows.Forms.Label label_phone;
+        private System.Windows.Forms.Label label_fullName;
+        private System.Windows.Forms.Label label_userID;
+        private System.Windows.Forms.TextBox textBox_userID;
+        private System.Windows.Forms.TextBox textBox_fullName;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_phone;
+        private System.Windows.Forms.Button button_editEmail;
+        private System.Windows.Forms.Button button_editPhone;
+        private System.Windows.Forms.Button button_editFname;
+        private System.Windows.Forms.Button button_deleteAcc;
     }
 }
